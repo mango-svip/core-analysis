a123eec70ed909f1dd15558adabdafa22f0364af
mango-svip <cheng0509@vip.qq.com>
2025-03-01 17:52:30 +0800

    ui优化

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,23 +3,23 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-		  colors: {
-			primary: "#007AFF",
-			secondary: "#34C759",
-		  },
-		  borderRadius: {
-			none: "0px",
-			sm: "4px",
-			DEFAULT: "8px",
-			md: "12px",
-			lg: "16px",
-			xl: "20px",
-			"2xl": "24px",
-			"3xl": "32px",
-			full: "9999px",
-			button: "8px",
-		  },
+			colors: {
+				primary: "#FF4D4F",
+				secondary: "#52C41A",
+			},
+			borderRadius: {
+				none: "0px",
+				sm: "4px",
+				DEFAULT: "8px",
+				md: "12px",
+				lg: "16px",
+				xl: "20px",
+				"2xl": "24px",
+				"3xl": "32px",
+				full: "9999px",
+				button: "8px",
+			},
 		},
-	  },
+	},
 	plugins: [require("daisyui")],
 }
