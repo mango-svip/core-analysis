0e401a184f336b7c869d04e8270dcdc86a8e3433
mango-svip <cheng0509@vip.qq.com>
2025-05-01 14:32:52 +0800

    support deno env

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL || Deno.env.get("SUPABASE_URL");
+const supabaseKey = import.meta.env.SUPABASE_KEY || Deno.env.get("SUPABASE_KEY");
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
