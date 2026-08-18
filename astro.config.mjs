6a2084f3cbaf05a66ee3b89796872cc8e5267fc9
mango-svip <cheng0509@vip.qq.com>
2026-08-18 23:45:46 +0800

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-import deno from "@astrojs/deno";
-
-import tailwind from "@astrojs/tailwind";
+import adapter from "@deno/astro-adapter";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno(),
-  integrations: [tailwind()]
+  adapter: adapter(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- src/pages/reports/[...report_id].ts
+++ src/pages/reports/[...report_id].ts
@@ -1,4 +1,6 @@
-export async function get({ params }) {
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params }) => {
     const report_id = params.report_id;
     const vip_response = await fetch("https://iu8xq68rii.execute-api.ap-southeast-1.amazonaws.com/default/vip-trade-analysis?report_id=" + report_id);
     const html = await vip_response.text();
